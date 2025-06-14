ff3b7c5f29db4aefa2bf4dc5e0e3b44626f43144
SofiaMarcano <sofia.marcano@udea.edu.co>
2025-06-14 06:39:58 -0500

    class dicomc

--- Clases.py
+++ Clases.py
@@ -10,3 +10,26 @@ class Paciente:
 
     def __str__(self):
         return f"Paciente {self.nombre} (ID: {self.id_paciente}, Edad: {self.edad})"
+class DICOMC:
+    def __init__(self, carpeta):
+        self.carpeta = carpeta
+        self.volumen = None
+        self.meta_info = None
+    def cargar_dicom_y_reconstruir(self):
+        arc = sorted([os.path.join(self.carpeta, f) for f in os.listdir(self.carpeta) if f.endswith(".dcm")])
+        imagenes = []
+        for i in arc:
+            ds = pydicom.dcmread(i)
+            imagenes.append(ds.pixel_array)
+            if self.meta_info is None:
+                self.meta_info = ds  # Guardamos un solo archivo con metadata
+        self.volumen = np.stack(imagenes, axis=0)
+        return self.volumen
+    def obt_info(self):
+        if self.meta_info:
+            nombre = self.meta_info.get('PatientName', 'Desconocido')
+            edad = self.meta_info.get('PatientAge', '00Y')
+            id_paciente = self.meta_info.get('PatientID', '0000')
+            return str(nombre), int(edad[:-1]), str(id_paciente)
+        return "Anonimo", 0, "0000"
+    
